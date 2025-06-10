6278aa94f1b8e4f03456f16d1e4d21652eab1ef5
ghostrider101218 <142645808+ghostrider101218@users.noreply.github.com>
2025-06-09 21:08:46 -0500

    chore: initial commit

---
+++ src/main/java/com/gostech/swiftportdocs/SwiftportDocsApplication.java
@@ -0,0 +1,13 @@
+package com.gostech.swiftportdocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftportDocsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwiftportDocsApplication.class, args);
+    }
+
+}
